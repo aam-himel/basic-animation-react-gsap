33dd639b5748f019d01f0666fed7bc5cb3f909ed
aamhimel <aam.himel@gmail.com>
2021-01-04 00:50:45 +0600

    basic animation done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import React, {useEffect, useRef} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+import {TweenMax, Power3} from 'gsap';
+
+
 function App() {
+
+  let imageItem = useRef(null);
+  let textAnimation = useRef(null);
+
+  useEffect(() => {
+    console.log(imageItem);
+    TweenMax.to(
+      imageItem,
+      .8,
+      {
+        opacity: 1,
+        y: -25,
+        ease: Power3.easeOut
+      }
+    )
+
+    TweenMax.to(
+      textAnimation,
+      .8,
+      {
+        opacity: 1,
+        y: -25,
+        delay: .3,
+        ease: Power3.easeOut
+      }
+
+    )
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img 
+        ref = {el => {imageItem = el}}
+        src={logo} className="App-logo" alt="logo" />
+        <p ref = {el => {textAnimation = el;}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
